00101a862e2a23b33e4250897153b7d803e1c365
MD Benjir Kamal <43960907+benjirkamal@users.noreply.github.com>
2026-04-26 20:14:08 +0600

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      disallow: "/",
       disallow: ["/api/", "/_next/"],
     },
     sitemap: `${SITE_CONFIG.url}/sitemap.xml`,
